497ea5384397117017277ee6f0abb70460c68f75
suyZhong <zsy43847@163.com>
2021-07-05 01:27:07 +0800

    finnish customer all func

--- customer.py
+++ customer.py
@@ -4,6 +4,7 @@ from PyQt5.Qt import QStackedLayout
 from PyQt5.QtWidgets import QWidget, QTableWidgetItem
 from sqlalchemy.exc import IntegrityError, DataError
 from PyQt5.Qt import QMessageBox
+from sqlalchemy import or_
 
 
 class CustomerUI(QWidget, Ui_customer):
@@ -13,8 +14,56 @@ class CustomerUI(QWidget, Ui_customer):
         self.setupUi(self)
         self.customerAdd.clicked.connect(self.addCustomer)
         self.CusQueryButton.clicked.connect(self.queryCustomer)
+        self.CusDelButton.clicked.connect(self.delCustomer)
+        self.CusQueryButton_3.clicked.connect(self.updateCustomer)
         self.CusID_2.textChanged.connect(self.queryCustomer)
+        self.CusID_3.textChanged.connect(self.queryCustomer)
+        self.CusID_4.textChanged.connect(self.queryCustomer)
         self.clearButton_2.clicked.connect(self.tableWidget.clearContents)
+        self.tableWidget.clicked.connect(self.setTextFromTable)
+
+    def getTableText(self, row, col):
+        try:
+            result = self.tableWidget.item(row, col).text()
+        except AttributeError:
+            result = ''
+        return result
+
+    def setTextFromTable(self):
+        if self.tabWidget.currentIndex() == 0:
+            return
+        if self.tabWidget.currentIndex() == 1:
+            return
+        row = self.tableWidget.currentRow()
+        id = self.getTableText(row, 0)
+        name = self.getTableText(row, 1)
+        phone = self.getTableText(row, 2)
+        address = self.getTableText(row, 3)
+        contact_name = self.getTableText(row, 4)
+        contact_phone = self.getTableText(row, 5)
+        contact_mail = self.getTableText(row, 6)
+        contact_relation = self.getTableText(row, 7)
+        employee = self.getTableText(row, 8)
+        if self.tabWidget.currentIndex() == 2:
+            self.CusID_3.setText(id)
+            self.CusName_3.setText(name)
+            self.CusPhone_3.setText(phone)
+            self.CusAddress_3.setText(address)
+            self.CusCname_3.setText(contact_name)
+            self.CusPhone_3.setText(contact_phone)
+            self.CusAddress_3.setText(contact_mail)
+            self.CusRela_3.setText(contact_relation)
+            self.CusEmployee2_3.setText(employee)
+        if self.tabWidget.currentIndex() == 3:
+            self.CusID_4.setText(id)
+            self.CusName_4.setText(name)
+            self.CusPhone_4.setText(phone)
+            self.CusAddress_4.setText(address)
+            self.CusCname_4.setText(contact_name)
+            self.CusPhone_4.setText(contact_phone)
+            self.CusAddress_4.setText(contact_mail)
+            self.CusRela_4.setText(contact_relation)
+            self.CusEmployee2_3.setText(employee)
 
     def addCustomer(self):
         id = self.CusAddID.text()
@@ -25,10 +74,14 @@ class CustomerUI(QWidget, Ui_customer):
         contact_phone = self.CusAddCphone.text()
         contact_mail = self.CusAddCaddr.text()
         contact_relation = self.CusAddRela.text()
+        # employee = self.CusEmployee.text()
+        employee = self.CusEmployee.currentText()
+
         try:
             db.addCustomer(self.engine, id, name, phone, address, contact_name, contact_phone, contact_mail,
-                           contact_relation)
+                           contact_relation, employee)
         except IntegrityError:
+            print(IntegrityError)
             msgBox = QMessageBox(QMessageBox.Warning, 'Error', 'The SQL is wrong')
             msgBox.exec_()
             return
@@ -44,14 +97,36 @@ class CustomerUI(QWidget, Ui_customer):
         session.close()
 
     def queryCustomer(self):
-        idd = '%' + self.CusID_2.text() + '%'
-        name = '%' + self.CusName_2.text() + '%'
-        phone = '%' + self.CusPhone_2.text() + '%'
-        address = '%' + self.CusAddress_2.text() + '%'
-        contact_name = '%' + self.CusCname_2.text() + '%'
-        contact_phone = '%' + self.CusCphone_2.text() + '%'
-        contact_mail = '%' + self.CusCaddr_2.text() + '%'
-        contact_relation = '%' + self.CusRela_2.text() + '%'
+        if self.tabWidget.currentIndex() == 1:
+            idd = '%' + self.CusID_2.text() + '%'
+            name = '%' + self.CusName_2.text() + '%'
+            phone = '%' + self.CusPhone_2.text() + '%'
+            address = '%' + self.CusAddress_2.text() + '%'
+            contact_name = '%' + self.CusCname_2.text() + '%'
+            contact_phone = '%' + self.CusCphone_2.text() + '%'
+            contact_mail = '%' + self.CusCaddr_2.text() + '%'
+            contact_relation = '%' + self.CusRela_2.text() + '%'
+            employee = '%' + self.CusEmployee2.text() + '%'
+        elif self.tabWidget.currentIndex() == 2:
+            idd = '%' + self.CusID_3.text() + '%'
+            name = '%' + self.CusName_3.text() + '%'
+            phone = '%' + self.CusPhone_3.text() + '%'
+            address = '%' + self.CusAddress_3.text() + '%'
+            contact_name = '%' + self.CusCname_3.text() + '%'
+            contact_phone = '%' + self.CusCphone_3.text() + '%'
+            contact_mail = '%' + self.CusCaddr_3.text() + '%'
+            contact_relation = '%' + self.CusRela_3.text() + '%'
+            employee = '%' + self.CusEmployee2_2.text() + '%'
+        elif self.tabWidget.currentIndex() == 3:
+            idd = '%' + self.CusID_4.text() + '%'
+            name = '%' + self.CusName_4.text() + '%'
+            phone = '%' + self.CusPhone_4.text() + '%'
+            address = '%' + self.CusAddress_4.text() + '%'
+            contact_name = '%' + self.CusCname_4.text() + '%'
+            contact_phone = '%' + self.CusCphone_4.text() + '%'
+            contact_mail = '%' + self.CusCaddr_4.text() + '%'
+            contact_relation = '%' + self.CusRela_4.text() + '%'
+            employee = '%' + self.CusEmployee2_3.text() + '%'
 
         session = db.sessionmaker(self.engine)()
         data = session.query(db.Customer).filter(db.Customer.id.like(idd),
@@ -61,7 +136,49 @@ class CustomerUI(QWidget, Ui_customer):
                                                  db.Customer.contact_name.like(contact_name),
                                                  db.Customer.contact_phone.like(contact_phone),
                                                  db.Customer.contact_email.like(contact_mail),
-                                                 db.Customer.contact_relation.like(contact_relation)).all()
+                                                 db.Customer.contact_relation.like(contact_relation),
+                                                 or_(db.Customer.empl_id.like(employee),
+                                                     db.Customer.empl_id.is_(None))).all()
+        self.showCustomer(data)
+        session.commit()
+        session.close()
+
+    def delCustomer(self):
+        id = self.CusID_3.text()
+        msgBox = QMessageBox(QMessageBox.Warning, 'Warning', 'Are you sure to delete? It is not invertible')
+        msgBox.exec_()
+        session = db.sessionmaker(self.engine)()
+        data = session.query(db.Customer).filter(db.Customer.id == id)
+        data.delete()
+        session.commit()
+        session.close()
+
+        session = db.sessionmaker(self.engine)()
+        data = session.query(db.Customer).all()
+        self.showCustomer(data)
+        session.commit()
+        session.close()
+
+    def updateCustomer(self):
+        id = self.CusID_4.text()
+        name = self.CusName_4.text()
+        phone = self.CusPhone_4.text()
+        address = self.CusAddress_4.text()
+        contact_name = self.CusCname_4.text()
+        contact_phone = self.CusCphone_4.text()
+        contact_mail = self.CusCaddr_4.text()
+        contact_relation = self.CusRela_4.text()
+        employee = self.CusEmployee2_3.text()
+        session = db.sessionmaker(self.engine)()
+        data = session.query(db.Customer).filter(db.Customer.id == id).update(
+            {'name': name, 'phone': phone, 'address': address, 'contact_name': contact_name,
+             'contact_phone': contact_phone, 'contact_email': contact_mail, 'contact_relation': contact_relation,
+             'empl_id': employee})
+        session.commit()
+        session.close()
+
+        session = db.sessionmaker(self.engine)()
+        data = session.query(db.Customer).all()
         self.showCustomer(data)
         session.commit()
         session.close()
@@ -78,3 +195,4 @@ class CustomerUI(QWidget, Ui_customer):
             self.tableWidget.setItem(i, 5, QTableWidgetItem(row.contact_phone))
             self.tableWidget.setItem(i, 6, QTableWidgetItem(row.contact_email))
             self.tableWidget.setItem(i, 7, QTableWidgetItem(row.contact_relation))
+            self.tableWidget.setItem(i, 8, QTableWidgetItem(row.empl_id))
--- db.py
+++ db.py
@@ -6,20 +6,85 @@ from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import relationship
 
 
+def initInsertion(engine):
+    session = sessionmaker(engine)()
+    session.add(Bank(name='Tiger bank', city='tiger', possession=1e25))
+    session.add(Bank(name='Lion bank', city='lion', possession=2e13))
+    session.add(Bank(name='Doggy bank', city='dog', possession=2500))
+    session.add(Bank(name='Eagle bank', city='eagle', possession=3e14))
+    session.add(Department(id='1234321', name='Face', manager_id='114514', bank_name='Tiger bank'))
+    session.add(Department(id='1234322', name='Nose', manager_id='114514', bank_name='Tiger bank'))
+    session.add(Department(id='1234323', name='Mouth', manager_id='114514', bank_name='Tiger bank'))
+    session.add(Department(id='1234324', name='Teeth', manager_id='114514', bank_name='Tiger bank'))
+    session.add(Department(id='1333321', name='Face', manager_id='114514', bank_name='Lion bank'))
+    session.add(Department(id='1333322', name='Nose', manager_id='114514', bank_name='Lion bank'))
+    session.add(Department(id='1333323', name='Mouth', manager_id='114514', bank_name='Lion bank'))
+    session.add(Department(id='1333324', name='Teeth', manager_id='114514', bank_name='Lion bank'))
+    session.add(Department(id='1444321', name='Face', manager_id='114514', bank_name='Doggy bank'))
+    session.add(Department(id='1444322', name='Nose', manager_id='114514', bank_name='Doggy bank'))
+    session.add(Department(id='1444323', name='Mouth', manager_id='114514', bank_name='Doggy bank'))
+    session.add(Department(id='1444324', name='Teeth', manager_id='114514', bank_name='Doggy bank'))
+    session.add(Department(id='5551321', name='Face', manager_id='114514', bank_name='Eagle bank'))
+    session.add(Department(id='5551322', name='Nose', manager_id='114514', bank_name='Eagle bank'))
+    session.add(Department(id='5551323', name='Mouth', manager_id='114514', bank_name='Eagle bank'))
+    session.add(Department(id='5551324', name='Teeth', manager_id='114514', bank_name='Eagle bank'))
+    session.add(Employee(id='12345', name='White Tiger1', phone='30303023', address='meng jia la',
+                begin_date='2020/9/12', bank_name='Tiger bank', depart_id='1234321', type='manager'))
+    session.add(Employee(id='12346', name='White Tiger2', phone='30303043', address='meng jia la',
+                begin_date='2020/9/12', bank_name='Tiger bank', depart_id='1234322', type='general'))
+    session.add(Employee(id='12347', name='White Tiger3', phone='30303054', address='meng jia la',
+                begin_date='2020/9/12', bank_name='Tiger bank', depart_id='1234323', type='general'))
+    session.add(Employee(id='12348', name='White Tiger4', phone='30303065', address='meng jia la',
+                begin_date='2020/9/12', bank_name='Tiger bank', depart_id='1234324', type='general'))
+
+    session.add(Employee(id='12349', name='White Eagle1', phone='30303012', address='America',
+                begin_date='2020/9/11', bank_name='Eagle bank', depart_id='1333321', type='manager'))
+    session.add(Employee(id='12350', name='White Eagle2', phone='30303034', address='America',
+                begin_date='2020/9/11', bank_name='Eagle bank', depart_id='1333322', type='general'))
+    session.add(Employee(id='12351', name='White Eagle3', phone='30303354', address='America',
+                begin_date='2020/9/11', bank_name='Eagle bank', depart_id='1333323', type='general'))
+    session.add(Employee(id='22351', name='White Eagle4', phone='30303354', address='America',
+                begin_date='2020/9/11', bank_name='Eagle bank', depart_id='1333324', type='general'))
+
+    session.add(Employee(id='14349', name='White Doggy1', phone='30212312', address='Japan',
+                begin_date='2020/8/6', bank_name='Doggy bank', depart_id='1333321', type='manager'))
+    session.add(Employee(id='14350', name='White Doggy2', phone='21233034', address='Japan',
+                begin_date='2020/8/6', bank_name='Doggy bank', depart_id='1333322', type='general'))
+    session.add(Employee(id='14351', name='White Doggy3', phone='21233354', address='Japan',
+                begin_date='2020/8/6', bank_name='Doggy bank', depart_id='1333323', type='general'))
+    session.add(Employee(id='24351', name='White Doggy4', phone='21233354', address='Japan',
+                begin_date='2020/8/6', bank_name='Doggy bank', depart_id='1333324', type='general'))
+
+    session.add(Employee(id='18349', name='White Lion', phone='24303012', address='Waganda',
+                begin_date='2020/9/11', bank_name='Lion bank', depart_id='1333321', type='manager'))
+    session.add(Employee(id='18350', name='White Lion', phone='24302434', address='Waganda',
+                begin_date='2020/9/11', bank_name='Lion bank', depart_id='1333322', type='general'))
+    session.add(Employee(id='18351', name='White Lion', phone='24303354', address='Waganda',
+                begin_date='2020/9/11', bank_name='Lion bank', depart_id='1333323', type='general'))
+    session.add(Employee(id='28351', name='White Lion', phone='24303354', address='Waganda',
+                begin_date='2020/9/11', bank_name='Lion bank', depart_id='1333324', type='general'))
+
+    session.commit()
+    session.close()
+
 def login(username: str, password: str, host: str, port: int, dbName: str):
     DB_URL = f'mysql+pymysql://{username}:{password}@{host}:{port}/{dbName}'
     return sql.create_engine(DB_URL)
 
-def addCustomer(engine ,id, name, phone, address, cname, cphone, cmail, crelation):
+def addCustomer(engine ,id, name, phone, address, cname, cphone, cmail, crelation, empl_id):
     session = sessionmaker(engine)()
-    session.add(Customer(id=id, name=name, phone=phone, address=address, contact_name = cname,contact_phone = cphone,contact_email = cmail, contact_relation = crelation))
+    if empl_id != '':
+        session.add(Customer(id=id, name=name, phone=phone, address=address, contact_name = cname,contact_phone = cphone,contact_email = cmail, contact_relation = crelation, empl_id=empl_id))
+    else:
+        session.add(Customer(id=id, name=name, phone=phone, address=address, contact_name = cname,contact_phone = cphone,contact_email = cmail, contact_relation = crelation))
     session.commit()
     session.close()
 
 engine = sql.create_engine(DB_URL)
 
+# baseClass = declarative_base(engine)
 baseClass = declarative_base()
-# baseClass.metadata.clear()
+baseClass.metadata.clear()
 
 
 class Bank(baseClass):
@@ -65,6 +130,7 @@ class Employee(baseClass):
     phone = sql.Column(sql.String(12))
     address = sql.Column(sql.String(50))
     begin_date = sql.Column(sql.Date)
+    type = sql.Column(sql.String(20))
 
     bank_name = sql.Column(sql.String(50), sql.ForeignKey('bank.name'))
     depart_id = sql.Column(sql.String(25), sql.ForeignKey('department.id'))
@@ -132,11 +198,12 @@ class Pay(baseClass):
 if __name__ == '__main__':
     engine = login('root', 'zsy123', 'localhost', '3306', 'bank_system')
     DBsession = sessionmaker(engine)
+    initInsertion(engine)
     session = DBsession()
     # session.add(Bank(name="zhossng guoen min bsu hang", city="hefias"))
     # session.add(Employee(id='1231223', name='null'))
-    data = session.query(Customer).filter(Customer.id.like('%'+'s'+'%')).all()
-    for i in data:
-        print(i.id)
+    # data = session.query(Customer).filter(Customer.id.like('%'+'s'+'%')).all()
+    # for i in data:
+        # print(i.id)
     session.commit()
     session.close()
